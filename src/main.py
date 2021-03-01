fc0b76be6e9629cf100157410ec29875b25495bd
Mario Campos Mocholí <mario.identation@gmail.com>
2021-03-01 21:13:46 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
-from input import Input
+# -*- coding: utf-8 -*-
+
+import numpy as np
 import pandas as pd
 import sys
 import re
 
 pattern_comments = re.compile(r'\(+(.*?)\)+')
-pattern_ray = re.compile(r'-+')
+pattern_ray = re.compile(r'\s*-+\s*')
+pattern_whitespace = re.compile(r'\s{2,}')
+pattern_dot = re.compile(r'\.\s*')
 
-def clean_description(txt):
-    return re.sub(pattern, '-', str(txt))
+def noise_removal(txt):
+    txt = str(txt).strip()
+    txt = txt.replace('\n', ' ').replace('\r', '')
+    txt = re.sub(pattern_comments, '', txt)
+    txt = re.sub(pattern_ray, '', txt)
+    txt = re.sub(pattern_whitespace, '', txt)
+    txt = re.sub(pattern_dot, '. ', txt)
+    return txt
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise(Exception("Numero de argumentos incorrecto"))
     else:
         df = pd.read_excel(sys.argv[1])
-        df['Description'].apply(clean_description)
+        df['Description'] = df['Description'].apply(noise_removal)
+        # df['Description'].replace('', np.nan, inplace=True)
+        # df.dropna(subset=['Description'], inplace=True)
+
+        for d in df['Description'].values:
+            print(d)
 
 
         
