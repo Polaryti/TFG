4e1136e2bdc093f20679a7c5a899e1237f477a41
Mario Campos Mocholí <mario.camposmocholi@gmail.com>
2021-03-04 15:41:01 +0100

    Update
    Update

--- src/main.py
+++ src/main.py
@@ -6,9 +6,14 @@ import sys
 import re
 
 pattern_comments = re.compile(r'\(+(.*?)\)+')
+pattern_comments_exhaustive = re.compile(r'\(+([\S\s]*)\)+')
 pattern_ray = re.compile(r'\s*-+\s*')
 pattern_whitespace = re.compile(r'\s{2,}')
-pattern_dot = re.compile(r'\.\s*')
+pattern_multiple_dot = re.compile(r'\.\s(\.\s)+|\.\.+')
+pattern_parentesis = re.compile(r'\(+|\)+')
+
+unique_txt = set()
+
 
 def noise_removal(txt):
     txt = str(txt).strip()
@@ -16,20 +21,43 @@ def noise_removal(txt):
     txt = re.sub(pattern_comments, '', txt)
     txt = re.sub(pattern_ray, '', txt)
     txt = re.sub(pattern_whitespace, '', txt)
-    txt = re.sub(pattern_dot, '. ', txt)
+    txt = re.sub(pattern_multiple_dot, '', txt)
+    txt = re.sub(pattern_parentesis, ' ', txt)
+
+    if txt.isupper() or txt in unique_txt or type(txt) != str or txt.isnumeric():
+        txt = np.nan
+    else:
+        unique_txt.add(txt)
+
     return txt
 
+
 if __name__ == "__main__":
+
     if len(sys.argv) != 2:
-        raise(Exception("Numero de argumentos incorrecto"))
+        raise(ValueError("Numero de argumentos incorrecto"))
     else:
         df = pd.read_excel(sys.argv[1])
+        print("Mostres abans del preprocessament: {}".format(len(df)))
+
         df['Description'] = df['Description'].apply(noise_removal)
-        # df['Description'].replace('', np.nan, inplace=True)
-        # df.dropna(subset=['Description'], inplace=True)
+        df['Description'].replace('', np.nan, inplace=True)
+        df.dropna(subset=['Description'], inplace=True)
+        df.dropna(subset=['Classificació'], inplace=True)
+        df.drop_duplicates(inplace=True)
+        df.drop(df.columns.difference(
+            ['Description', 'Classificació']), 1, inplace=True)
 
-        for d in df['Description'].values:
-            print(d)
+        unique_cat = []
+        aux = set()
 
+        for s in df['Classificació']:
+          unique_cat += (s.split('|'))
 
-        
+        for s in unique_cat:
+          aux.add(s.strip())
+
+        print("Mostres després del preprocessament: {}".format(len(df)))
+        print("Nombre total de clases: {}".format(len(aux)))
+
+        df.to_csv('res/data_01.csv')
